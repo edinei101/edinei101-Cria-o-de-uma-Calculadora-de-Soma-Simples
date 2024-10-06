0d647de84ab1024953eed92144b7e9c73505d3dd
edinei101 <116923846+edinei101@users.noreply.github.com>
2024-10-06 14:49:20 -0400

    Add files via upload

---
+++ Calculadora Simples/main.js
@@ -0,0 +1,11 @@
+
+
+function somar() {
+    var valor1 = document.getElementById("valor1").value;
+    var valor2 = document.getElementById("valor2").value;
+
+    var soma = parseFloat(valor1) + parseFloat(valor2);
+    
+    alert("O resuldado é: "  + soma);
+}
+
